5eae56e9554829507f3de53293606e24ebb47f26
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-01-26 00:13:27 +0100

    change

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./components/Home";
 import reportWebVitals from "./reportWebVitals";
-import {  Routes, Route, HashRouter } from "react-router-dom";
+import {  Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import Destination from "./components/Destination";
 import Crew from "./components/Crew";
 import Technology from "./components/Technology";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/space-website" exact element={<Home />} />
       <Route path="/space-website/destination" exact element={<Destination/>} />
       <Route path="/space-website/crew" exact element={<Crew />} />
       <Route path="/space-website/tech" exact element={<Technology />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
